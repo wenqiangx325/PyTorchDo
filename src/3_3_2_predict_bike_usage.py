a9228013f93ee1ff2632b30c9045c6b3dbefdf3c
Wenqiang Xie <wq.xie@outlook.com>
2020-09-09 08:35:28 +0800

    remove a useless func download_data

--- src/3_3_2_predict_bike_usage.py
+++ src/3_3_2_predict_bike_usage.py
@@ -1,9 +1,5 @@
 import requests
 import torch
 
-def download_data():
-    url = "https://s3.amazonaws.com/capitalbikeshare-data/2017-capitalbikeshare-tripdata.zip"
-    requests.get(url)
-
 if __name__ == "__main__":
     pass
